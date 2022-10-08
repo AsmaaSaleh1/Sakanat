4231d0799ed7d3a47a2edf07c989b8f3e6427b3a
Ruba <s12028675@stu.najah.edu>
2022-10-08 15:52:00 +0300

    sakanat

---
+++ src/java/login.js
@@ -0,0 +1,44 @@
+document.querySelectorAll('input').forEach((item) => {
+    item.addEventListener("focus", function () {
+        item.previousElementSibling.className = 'label-selected';
+    })
+    item.addEventListener("blur", function () {
+        console.log(item.value);
+        if (item.value === '') {
+            item.previousElementSibling.className = '';
+        }
+    })
+})
+
+document.getElementById("registerLink").addEventListener("click", function () {
+    if (window.innerWidth < 600) {
+        document.getElementById("signUp").style.display = 'block';
+        document.getElementById("login").style.display = 'none';
+    }
+    else {
+        document.getElementById("overlay").style.transform = 'translate(550px , -25px)';
+        // document.getElementById("overlay").style.borderRadius='300px 25px 25px 25px';
+    }
+})
+document.getElementById("loginLink").addEventListener("click", function () {
+    if (window.innerWidth < 600) {
+        document.getElementById("login").style.display = 'block';
+        document.getElementById("signUp").style.display = 'none';
+    }
+    else {
+        document.getElementById("overlay").style.transform = 'translate(0px , -25px)';
+        // document.getElementById("overlay").style.borderRadius='25px 300px 25px 25px';
+    }
+})
+const loader=document.querySelector('.load');
+const main=document.querySelector('.main');
+function inIt(){
+    setTimeout(()=>{
+        loader.style.display ='none';
+        main.style.display ='flex';
+        setTimeout(()=>(main.style.opacity=1),50);
+
+    },2000);
+}
+inIt();
+
