5277120b92c17cab6bc60bb9ff9561054827ecf5
Asmaa <s12028417@stu.najah.edu>
2022-11-29 12:50:09 +0200

    prof

--- src/prof.php
+++ src/prof.php
@@ -20,10 +20,11 @@
                     <a href="#">
                         <img src="img/userprofile.png" alt="">
                     </a>
-                    <h1>Mis.Asmaa</h1>
+                    <h1>Mis.<span>Asmaa</span></h1> <!--Name come from database-->
                 </div>
                 <ul class="nav nav-pills nav-stacked">
                     <li class="active"><a href="#"> <i class="fa fa-user"></i> Profile</a></li>
+                    <!--TODO: the number of Recent Activity is counter for user Sakanat-->
                     <li><a href="#"> <i class="fa fa-calendar"></i> Recent Activity <span class="label label-warning pull-right r-activity">9</span></a></li>
                     <li><a href="#"> <i class="fa fa-edit"></i> Edit profile</a></li>
                 </ul>
@@ -38,28 +39,36 @@
                     <h1>Bio Graph</h1>
                     <div class="row">
                         <div class="bio-row">
-                            <p><span>First Name </span>: Camila</p>
+                            <p><span>First Name </span>: Asmaa</p><!--Name come from database-->
                         </div>
                         <div class="bio-row">
-                            <p><span>Last Name </span>: Smith</p>
+                            <p><span>Last Name </span>: Saleh</p><!--Name come from database-->
                         </div>
                         <div class="bio-row">
-                            <p><span>Country </span>: Australia</p>
+                            <p><span>ID </span>: 12028417</p><!--ID come from database-->
                         </div>
                         <div class="bio-row">
-                            <p><span>Birthday</span>: 13 July 1983</p>
+                            <p><span>Birthday</span>: 23 June 2002</p><!--Birthdate come from database-->
                         </div>
                         <div class="bio-row">
-                            <p><span>Occupation </span>: UI Designer</p>
+                            <p><span>City </span>: Nablus</p><!--City come from database-->
                         </div>
                         <div class="bio-row">
-                            <p><span>Email </span>: jsmith@flatlab.com</p>
+                            <p><span>Street </span>: Amman Street</p><!--street come from database-->
                         </div>
                         <div class="bio-row">
-                            <p><span>Mobile </span>: (12) 03 4567890</p>
+                            <p><span>Email </span>: asmaatareq1999@gmail.com</p><!--email come from database-->
                         </div>
                         <div class="bio-row">
-                            <p><span>Phone </span>: 88 (02) 123456</p>
+                            <p><span>Mobile </span>: 0592676710</p><!--mobile come from database-->
+                        </div>
+                        <div class="bio-row">
+                            <p><span>Phone </span>: 2321209</p><!--P.N come from database-->
+                        </div>
+                        <div class="bio-row">
+                            <!--TODO: but limitation about number of char for example 200char-->
+                            <!--Description come from database-->
+                            <p><span>Description </span>: Description Description Description Description Description</p>
                         </div>
                     </div>
                 </div>
