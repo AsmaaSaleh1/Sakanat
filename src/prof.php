9e8c8017c0c528ebae0ffb26feebdabe5fe91bfa
Asmaa <s12028417@stu.najah.edu>
2022-11-29 09:57:47 +0200

    prof

---
+++ src/prof.php
@@ -0,0 +1,73 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Profile</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="Css/prof.css">
+</head>
+<body>
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+<div class="container bootstrap snippets bootdey">
+    <div class="row">
+        <div class="profile-nav col-md-3">
+            <div class="panel">
+                <div class="user-heading round">
+                    <a href="#">
+                        <img src="img/userprofile.png" alt="">
+                    </a>
+                    <h1>Mis.Asmaa</h1>
+                </div>
+                <ul class="nav nav-pills nav-stacked">
+                    <li class="active"><a href="#"> <i class="fa fa-user"></i> Profile</a></li>
+                    <li><a href="#"> <i class="fa fa-calendar"></i> Recent Activity <span class="label label-warning pull-right r-activity">9</span></a></li>
+                    <li><a href="#"> <i class="fa fa-edit"></i> Edit profile</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="profile-info col-md-9">
+            <div class="panel">
+                <div class="bio-graph-heading">
+                    I'm here because I'm the best and I want the best.
+                </div>
+                <div class="panel-body bio-graph-info">
+                    <h1>Bio Graph</h1>
+                    <div class="row">
+                        <div class="bio-row">
+                            <p><span>First Name </span>: Camila</p>
+                        </div>
+                        <div class="bio-row">
+                            <p><span>Last Name </span>: Smith</p>
+                        </div>
+                        <div class="bio-row">
+                            <p><span>Country </span>: Australia</p>
+                        </div>
+                        <div class="bio-row">
+                            <p><span>Birthday</span>: 13 July 1983</p>
+                        </div>
+                        <div class="bio-row">
+                            <p><span>Occupation </span>: UI Designer</p>
+                        </div>
+                        <div class="bio-row">
+                            <p><span>Email </span>: jsmith@flatlab.com</p>
+                        </div>
+                        <div class="bio-row">
+                            <p><span>Mobile </span>: (12) 03 4567890</p>
+                        </div>
+                        <div class="bio-row">
+                            <p><span>Phone </span>: 88 (02) 123456</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
--- src/profile.php
+++ src/profile.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+include "nav.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
